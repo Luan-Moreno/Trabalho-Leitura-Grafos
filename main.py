1758d12e4cc226ef41f6d75b63d4ade7968f22d7
Luan Moreno <luanpmoreno@hotmail.com>
2022-11-05 06:22:59 -0300

    correção grafo completo

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Indique, também, entre que vértices as arestas múltipas ocorrem;  ////
 
 2-qual sequência dos graus do grafo? ////
 3-qual o número de arestas do grafo? ////
-4-o grafo é completo?
+4-o grafo é completo? ////
 5-o grafo é regular? ////
 6-o grafo é bipartido? Em caso afirmativo dê uma bipartição dos vértices do grafo.
 7-o grafo é bipartido completo? Em caso afirmativo dê uma bipartição dos vértices do grafo
@@ -28,16 +28,16 @@ No código poderá ser usada, no máximo, uma função.
 [1 2 3 4 5 6 7 8 9 10 11]
 '''
 
-lista = [];
-graus = [];
-simples = True;
-completo = True;
-regular = True;
-bipartido = True;
-bipartido_completo = True;
-resposta = "Nulo";
-grau = 0;
-arestas = 0;
+lista = []
+graus = []
+simples = True
+completo = True
+regular = True
+bipartido = True
+bipartido_completo = True
+resposta = "Nulo"
+grau = 0
+arestas = 0
 
 arquivo = open("A.txt", "r");
 
@@ -107,15 +107,15 @@ graus.remove(0)
 print("Os graus dos vértices são:")
 for x in range(len(graus)):
     print(graus[x], end=" ")
-    arestas += graus[x];
+    arestas += graus[x]
 
-arestas /= 2;
+arestas /= 2
 
 print(f"\nO número de arestas é: {arestas}")
 #################################
 '''
-4-o grafo é completo? /(X)
-5-o grafo é regular?  ///
+4-o grafo é completo? ////
+5-o grafo é regular?  ////
 6-o grafo é bipartido? Em caso afirmativo dê uma bipartição dos vértices do grafo.
 7-o grafo é bipartido completo? Em caso afirmativo dê uma bipartição dos vértices do grafo
 
@@ -123,9 +123,11 @@ No código poderá ser usada, no máximo, uma função.
 [1 2 3 4 5 6 7 8 9 10 11]
 '''
 
-for x in range(len(graus)): #Lógica não tá certa ainda (Dá pra ver no teste em A.txt)
-    if(graus[x] != len(lista) - 1):
-        completo = False
+for e in range(len(lista)):
+    for ee in range(len(lista[e])):
+        if(ee != e):
+            if(lista[e][ee] != 1):
+                completo = False
 
 if(completo == False):
     resposta = "Não."
